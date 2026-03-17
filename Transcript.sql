b7844e8e108cc5e59e2948199dac6c9ff8ca87af
michellerosenstock <michellerosenstock05@gmail.com>
2026-03-17 18:48:55 -0400

    V1 Creates Transcript_table uses Varchar (#2)
    Created the Transcript_table with specified columns. 
    uses Varchar instead of Varchar2.

--- Transcript.sql
+++ Transcript.sql
@@ -1,4 +1,11 @@
---create an SQL database consisting of 1 table
-  --table has no data initially.
-  --table has year, course code, course title, credit grade columns
---fill in the data using the values in a csv file.
+--create an SQL database consisting of 1 table. 
+--pull values from a csv file to populate blank SQL table.
+
+
+CREATE TABLE dbo.Transcript_table (
+  courseID INT PRIMARY KEY,
+  YearCompleted YEAR NOT NULL,
+  CourseTitle VARCHAR(50) NOT Null,
+  Credit DECIMAL(3,2) NOT NULL,
+  LetterGrade CHAR(1), NOT NULL
+);
