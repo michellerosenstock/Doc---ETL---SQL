69368f95cc9b0bbc0ce570a190760116e27ec4c9
michellerosenstock <michellerosenstock05@gmail.com>
2026-03-23 12:58:07 -0400

    deleted contents because will connect too PosgreSQL

--- Transcript.sql
+++ Transcript.sql
@@ -1,11 +1,3 @@
 --create an SQL database consisting of 1 table. 
 --pull values from a csv file to populate blank SQL table.
 
-
-CREATE TABLE dbo.Transcript_table (
-  courseID INT PRIMARY KEY,
-  YearCompleted YEAR NOT NULL,
-  CourseTitle VARCHAR(50) NOT Null,
-  Credit DECIMAL(3,2) NOT NULL,
-  LetterGrade CHAR(1), NOT NULL
-);
