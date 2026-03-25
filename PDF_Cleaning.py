ec15f3e8af328ab38033fa75d2d68c9377a26057
michellerosenstock <michellerosenstock05@gmail.com>
2026-03-25 19:04:05 -0400

    deleted sql file

--- PDF_Cleaning.py
+++ PDF_Cleaning.py
@@ -1,3 +1,4 @@
 # loads PDF
 # extracts transcript from PDF
 # places transxcript into CSV file.
+# sql tutorial for csv to table -using this video as a resource: https://www.youtube.com/watch?v=7LtwHSilDfU
