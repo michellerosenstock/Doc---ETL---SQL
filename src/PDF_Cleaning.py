e9aca40a27c0ba59d4be3e24cec5e1598662350b
michellerosenstock <michellerosenstock05@gmail.com>
2026-04-07 15:23:25 -0400

    deleted the python section.

--- src/PDF_Cleaning.py
+++
@@ -1,5 +0,0 @@
-# loads PDF
-# extracts transcript from PDF
-# places transxcript into CSV file.
-# sql tutorial for csv to table -using this video as a resource: https://www.youtube.com/watch?v=7LtwHSilDfU
-copy FROM '/local/path/to/data.pdf' WITH (FORMAT pdf);
