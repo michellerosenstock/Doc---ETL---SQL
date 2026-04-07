789cbfbcf9c0d1607adf6e3f38b0562069eb3cb6
michellerosenstock <michellerosenstock05@gmail.com>
2026-04-07 18:13:10 -0400

    modified data types: now all text.

--- SQL Source/create_staging_table_from_csv.sql
+++ SQL Source/create_staging_table_from_csv.sql
@@ -1,8 +1,8 @@
 CREATE TABLE staging_table (
     course_title TEXT,
     course_code TEXT,
-    year_completed INT,
-    credit FLOAT,
+    year_completed TEXT,
+    credit TEXT,
     grade TEXT
 );
 
