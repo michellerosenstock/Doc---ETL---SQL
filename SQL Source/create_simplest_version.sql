3dbc6d1777a9fbaa61ada8729bc3e40cd6871dc3
michellerosenstock <michellerosenstock05@gmail.com>
2026-04-13 19:19:44 -0400

    deleted the other sql files. the database, schema, and table are all placed in this 1 file for simplicity.

---
+++ SQL Source/create_simplest_version.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE transform_transcriptdb;
+
+CREATE SCHEMA transform_transcriptdb_schema;
+
+create TABLE  transform_transcriptdb_schema.transcript (
+    id BIGINT GENERATED ALWAYS AS IDENTITY (START WITH 1 INCREMENT BY 1) PRIMARY KEY,
+    course_title TEXT NOT NULL,
+    course_code INT NOT NULL CHECK (course_code > 0 AND course_code < 10000),
+    year_completed INT NOT NULL CHECK (year_completed BETWEEN 1600 AND 2500),
+    credit DECIMAL(5,2),
+    grade VARCHAR(2) NOT NULL CHECK (grade IN ('A', 'A-', 'B+', 'B', 'B-', 'C+', 'C', 'C-', 'D+', 'D', 'D-', 'F')),
+    
+)
