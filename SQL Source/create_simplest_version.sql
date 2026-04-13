31ceaab9e702f1772d192f94fdf720146ce1ad78
michellerosenstock <michellerosenstock05@gmail.com>
2026-04-13 19:32:52 -0400

    indented the items in the "insert into" clause.

--- SQL Source/create_simplest_version.sql
+++ SQL Source/create_simplest_version.sql
@@ -14,17 +14,18 @@ create TABLE  transform_transcriptdb_schema.transcript (
 
 insert into transform_transcriptdb_schema.transcript
 
-(course_title, course_code, year_completed, credit, grade) values
-('Introduction to Computer Science', 101, 2020, 3.00, 'A'),
-('Data Structures', 201, 2021, 3.00, 'B+'),
-('Algorithms', 301, 2022, 3.00, 'A-'),
-('Database Systems', 401, 2023, 3.00, 'B'),
-('Operating Systems', 501, 2024, 3.00, 'C+'),
-('Communication Skills', 102, 2020, 2.00, 'A'),
-('Calculus I', 202, 2021, 4.00, 'B-'),
-('Calculus II', 302, 2022, 4.00, 'C'),
-('Linear Algebra', 402, 2023, 3.00, 'B+'),
-('Statistics', 502, 2024, 3.00, 'A'),
-('Artificial Intelligence', 601, 2024, 3.00, 'A');
+    (course_title, course_code, year_completed, credit, grade) values
+    ('Introduction to Computer Science', 101, 2020, 3.00, 'A'),
+    ('Data Structures', 201, 2021, 3.00, 'B+'),
+    ('Algorithms', 301, 2022, 3.00, 'A-'),
+    ('Database Systems', 401, 2023, 3.00, 'B'),
+    ('Operating Systems', 501, 2024, 3.00, 'C+'),
+    ('Communication Skills', 102, 2020, 2.00, 'A'),
+    ('Calculus I', 202, 2021, 4.00, 'B-'),
+    ('Calculus II', 302, 2022, 4.00, 'C'),
+    ('Linear Algebra', 402, 2023, 3.00, 'B+'),
+    ('Statistics', 502, 2024, 3.00, 'A'),
+    ('Artificial Intelligence', 601, 2024, 3.00, 'A');
+    
 
 SELECT * FROM transform_transcriptdb_schema.transcript;
