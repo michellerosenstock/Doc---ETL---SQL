673724725a0ce7f625a45c434ef7c10241ae5a85
michellerosenstock <michellerosenstock05@gmail.com>
2026-04-20 16:58:08 -0400

    Implement main method for PDF management
    Added main method to manage_pdf.cs for testing.

--- manage_pdf.cs
+++ manage_pdf.cs
@@ -1,3 +1,6 @@
+using System;
+using System.io;
+
 /*
 input: pdf file object
 output: boolean value true or false
@@ -7,3 +10,8 @@ if PDF exists, return true
 else return false
 
 */
+public static void main (){
+
+  System.console(
+"Hello world"  
+}
