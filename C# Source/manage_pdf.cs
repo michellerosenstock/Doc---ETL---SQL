d28c19e569877023df89affff5fed1c78cc37ed6
michellerosenstock <michellerosenstock05@gmail.com>
2026-04-21 18:02:47 -0400

    Change PDF existence check to use a URL
    Updated the PDF source link and modified logic to check for the existence of a PDF URL.

--- C# Source/manage_pdf.cs
+++ C# Source/manage_pdf.cs
@@ -1,17 +1,32 @@
 using System;
+using System.IO;
+
 
 
 // a source on reading PDFs in C#: https://dev.to/eliotjones/reading-a-pdf-in-c-on-net-core-43ef
+// PDF raw link: https://raw.githubusercontent.com/michellerosenstock/Doc---ETL---SQL/main/C%23%20Source/Transcript.pdf
+
 
 
 class Main_Class {
 
 /*
-input: pdf file object
+
+Logic Case 1: File exists:
+input: pdf
 output: boolean value true or false
 
 
 if PDF exists, return true
+else return false
+
+
+Logic Case 2: File has content:
+input: pdf
+output: boolean value true or false
+
+if PDF has a table,
+return true
 else return false
 
 */
@@ -20,15 +35,19 @@ else return false
 static void Main(string[] args){
 
 
-  bool pdfExists = false;
-  String pdf = "sample_pdf.pdf";
+  bool pdfURLExists = false;
+  String pdfURL = https://raw.githubusercontent.com/michellerosenstock/Doc---ETL---SQL/main/C%23%20Source/Transcript.pdf
 
-  if (pdf != null){
-    pdfExists = true;
+ //"sample_pdf.pdf"; updating it twith the actual raw PDF link. storing it as a string
+
+  
+
+  if (pdfURL != null){
+    pdfURLExists = true;
   }
  
   
- Console.WriteLine("Does PDF exist? " + pdfExists);
+ Console.WriteLine("Does PDF exist? " + pdfURLExists);
     
     }
 
