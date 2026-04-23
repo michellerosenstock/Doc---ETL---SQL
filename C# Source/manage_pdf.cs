5f5acdee1ac16f019a1076dd35da990edf3a075c
michellerosenstock <michellerosenstock05@gmail.com>
2026-04-23 08:10:00 -0400

    modified .csproj file and .gitignore file

--- C# Source/manage_pdf.cs
+++ C# Source/manage_pdf.cs
@@ -8,35 +8,35 @@ using System.IO;
 
 
 
-class Main_Class {
+public class Main_Class {
 
-/*
+    /*
 
-Logic Case 1: File exists:
-input: pdf
-output: boolean value true or false
-
-
-if PDF exists, return true
-else return false
+    Logic Case 1: File exists:
+    input: pdf
+    output: boolean value true or false
 
 
-Logic Case 2: File has content:
-input: pdf
-output: boolean value true or false
-
-if PDF has a table,
-return true
-else return false
-
-*/
+    if PDF exists, return true
+    else return false
 
 
-static void Main(string[] args){
+    Logic Case 2: File has content:
+    input: pdf
+    output: boolean value true or false
+
+    if PDF has a table,
+    return true
+    else return false
+
+    */
+
+
+    static void Main(string[] args){
 
 
   bool pdfURLExists = false;
-  String pdfURL = https://raw.githubusercontent.com/michellerosenstock/Doc---ETL---SQL/main/C%23%20Source/Transcript.pdf
+  String pdfURL = "https://raw.githubusercontent.com/michellerosenstock/Doc---ETL---SQL/main/C%23%20Source/Transcript.pdf";
 
  //"sample_pdf.pdf"; updating it twith the actual raw PDF link. storing it as a string
 
